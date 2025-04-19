b9df91d87b80e98a7c1c543fc5f8d85f080c9032
Andrade <andrade.nicolas.melo@outlook.com>
2025-04-19 18:13:17 -0300

    Adicionar arquivos de projeto.

---
+++ jogo-dos-dados.ConsoleApp1/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace jogo_dos_dados.ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
